f1924a12b4f64631a1a2ff65dbff969b9944d125
Zohaab Khan <zohaabkhan@Zohaabs-Air.wifi.gsu.edu>
2022-03-29 15:19:07 -0400

    feature commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import flask
+import requests
+import os
+
+app = flask.Flask(__name__)
+
+MY_API_SECRET_KEY = "x95xd2dbxc3x175md5"
+
+BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+
+params = {
+    "q": "braves",  # Query keywords
+    "api-key": os.getenv(MY_API_SECRET_KEY),
+}
+
+response = requests.get(BASE_URL, params=params)
+data = response.json()
+headlines = []
+
+for i in range(0, 10):
+    print(data["response"]["docs"][i]["headline"]["main"])
+    headlines.append(data["response"]["docs"][i]["headline"]["main"])
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html", headlines=headlines)
+
+
+app.run()
