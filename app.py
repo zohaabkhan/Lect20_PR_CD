431fa027ac9956721d2557addc5a6bba7f704c79
Monica <mbyrd3@student.gsu.edu>
2022-03-29 16:17:37 -0400

    created conflict

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ data = response.json()
 headlines = []
 
 for i in range(0, 10):
-    print(data['response']['docs'][i]['headline']['main'])
+    return(data['response']['docs'][i]['headline']['main'])
     headlines.append(data['response']['docs'][i]['headline']['main'])
 
 @app.route("/")
